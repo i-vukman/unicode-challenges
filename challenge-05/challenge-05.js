265ba79abe15d5c2ce8a1aa0e748f606e196949a
Ivan Vukman <ivan.vukman@gmail.com>
2024-04-29 00:14:07 +0200

    Make challenge-05

---
+++ challenge-05/challenge-05.js
@@ -0,0 +1,6 @@
+// Using upper and lowercasing, try to determine if following string pairs are equal. (assume all strings are already normalized)
+// strasse - straße
+// straẞe - straße  
+// straSSe - straẞe
+// ΣTΣTΣ - σtσtς (context sensitive example)
+// İme - Ime (try turkish locale)
