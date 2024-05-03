e3de301513fbc1c26ebfdafde5b1d26f02b5d005
Ivan Vukman <ivan.vukman@gmail.com>
2024-05-04 01:40:37 +0200

    Make challenge-08

---
+++ challenge-08/Program.cs
@@ -0,0 +1,6 @@
+﻿// 1. Prove "aa" would match "å" in a Danish tailoring of the UCA and "ß" would match against "ss" in a German tailoring of the UCA
+
+// 2. Sort array of strings ["z", "ö"] in a German and Swedish tailoring of the UCA
+
+
+Console.WriteLine(["z", "ö"].Sort());
