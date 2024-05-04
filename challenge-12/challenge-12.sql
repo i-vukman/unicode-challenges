22cbd6c624b1c02a78a79ca21966b0cca705c3f3
Ivan Vukman <ivan.vukman@gmail.com>
2024-05-04 02:56:40 +0200

    Make challenge-12

---
+++ challenge-12/challenge-12.sql
@@ -0,0 +1,11 @@
+/* Create pg_trgm extension using the following query */
+
+CREATE EXTENSION pg_trgm;
+
+/* 1. Using show_trgm function, display trigrams of 'word' and 'two words' */
+
+SELECT show_trgm(''), show_trgm('');
+
+/* 2. Using word_similarity find the highest similarity of 'word' to any substring in 'two words' */
+
+/* 3. Using strict_word_similarity find the similarity of 'word' to any whole word in 'two words' */
