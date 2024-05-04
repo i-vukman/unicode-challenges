d35ad127f13295b6d0b7b53e1563ec711360a258
Ivan Vukman <ivan.vukman@gmail.com>
2024-05-04 02:16:45 +0200

    Make challenge-10

---
+++ challenge-10/challenge-10.sql
@@ -0,0 +1,10 @@
+/* Create two nondeterministic level-1 icu collations, da and und */
+
+/* 1. Using da collation, check that "AA" would match "å"*/
+
+SELECT 'AA' = 'å';
+
+/* 2. Using und collation, check that Č matches c and ć */
+
+SELECT 'Č' = 'c';
+SELECT 'Č' = 'ć';
