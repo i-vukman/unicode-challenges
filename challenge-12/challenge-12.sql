03459d8e894ceb708e8cc6b9aae7635d2186e28f
Ivan Vukman <ivan.vukman@gmail.com>
2024-05-04 03:05:15 +0200

    Update challenge-12

--- challenge-12/challenge-12.sql
+++ challenge-12/challenge-12.sql
@@ -6,6 +6,8 @@ CREATE EXTENSION pg_trgm;
 
 SELECT show_trgm(''), show_trgm('');
 
+/* 2. Using similarity find the similarity of 'word' and 'two words' */
+
 /* 2. Using word_similarity find the highest similarity of 'word' to any substring in 'two words' */
 
 /* 3. Using strict_word_similarity find the similarity of 'word' to any whole word in 'two words' */
