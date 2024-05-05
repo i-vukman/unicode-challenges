4cf7498224641d8c1f131d8ea389a468a5ebc78b
Ivan Vukman <ivan.vukman@gmail.com>
2024-05-05 18:44:56 +0200

    Update challenge-02.2

--- challenge-02/challenge-02.2/Program.cs
+++ challenge-02/challenge-02.2/Program.cs
@@ -5,3 +5,7 @@ var toReverse = "Rasistički";
 var reversed = ""; // TODO reverse toReverse using grapheme clusters https://learn.microsoft.com/en-us/dotnet/standard/base-types/character-encoding-introduction
 
 Console.WriteLine(reversed);
+
+// Print number of letters in string
+
+var text = "Rasistički";
